1296c2ed96eca9f5b187fb989de8ef63a3c69150
savalet <savinien.petitjean@gmail.com>
2024-12-02 17:48:14 +0100

    fix: reverse qsort

--- src/utils/compare.c
+++ src/utils/compare.c
@@ -12,7 +12,7 @@ int compare_pid(const void *first, const void *second)
     proc_info_t first_int = *(proc_info_t *)first;
     proc_info_t second_int = *(proc_info_t *)second;
 
-    return second_int.pid - first_int.pid;
+    return first_int.pid - second_int.pid;
 }
 
 int compare_reverse_pid(const void *first, const void *second)
@@ -20,5 +20,5 @@ int compare_reverse_pid(const void *first, const void *second)
     proc_info_t first_int = *(proc_info_t *)first;
     proc_info_t second_int = *(proc_info_t *)second;
 
-    return first_int.pid - second_int.pid;
+    return second_int.pid - first_int.pid;
 }
